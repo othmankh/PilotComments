f90c10629dd4411fb0be8ed3f601d2a9a973c85c
othmankh <othmankhamra@gmail.com>
2017-03-22 21:15:33 +0200

    all heroku related changes

--- heroku-web.js
+++ heroku-web.js
@@ -1,4 +1,4 @@
-var gzippo = require('gzippo');
+/*var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
@@ -7,4 +7,28 @@ var fs = require('fs');
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
 app.use(morgan('combinedstream: accessLogStream}));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000); */
+
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
