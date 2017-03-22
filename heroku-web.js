804bd8e2d952e2561713af2a9e731734fc13c774
othmankh <othmankhamra@gmail.com>
2017-03-22 22:43:42 +0200

    all heroku related changes

--- heroku-web.js
+++ heroku-web.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
 app.use(express.static(__dirname + '/dist'));
-app.set('view engine', 'ejs');
-var port = process.env.PORT || 8080;
 
+app.set('view engine', 'ejs');
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
-});
-
-// set the home page route
-app.get('/', function(req, res) {
-	res.render('index');
 });
 
 
