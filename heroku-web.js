3baf4e1d68a31994b8a159f69b8c8a6fd64ca334
othmankh <othmankhamra@gmail.com>
2017-03-22 21:38:35 +0200

    all heroku related changes

--- heroku-web.js
+++ heroku-web.js
@@ -1,26 +1,27 @@
-/*var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-var morgan = require('morgan');
-var logger = morgan('combined');
 var fs = require('fs');
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
-app.use(morgan('combinedstream: accessLogStream}));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000); */
+app.use(express.static(__dirname + '/dist'));
 
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
+
+app.set('view engine', 'jade');
+
+
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
+
+*/
 
 // set the home page route
 app.get('/', function(req, res) {
@@ -28,6 +29,8 @@ app.get('/', function(req, res) {
 	// ejs render automatically looks in the views folder
 	res.render('index');
 });
+
+var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
