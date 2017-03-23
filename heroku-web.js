67a885168248e8b26ab8c93cd5a52b3ebfa62a6b
othmankh <othmankhamra@gmail.com>
2017-03-23 17:36:32 +0200

    make it better

--- heroku-web.js
+++ heroku-web.js
@@ -4,7 +4,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.set('view engine', 'ejs');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 443;
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
